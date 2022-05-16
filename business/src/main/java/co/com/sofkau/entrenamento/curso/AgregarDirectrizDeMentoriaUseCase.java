8d0554baa7f5848180b8989b680b54b1bebde7e0
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-16 17:53:10 -0500

    Actualizando clases
    Se actualizaron las clases para agregarDirectriz

--- business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCase.java
+++ business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCase.java
@@ -3,11 +3,20 @@ package co.com.sofkau.entrenamento.curso;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofkau.entrenamiento.curso.Curso;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
 
-public class AgregarDirectrizDeMentoriaUseCase extends UseCase<RequestCommand<>, ResponseEvents> {
+public class AgregarDirectrizDeMentoriaUseCase extends UseCase<RequestCommand<AgregarDirectrizMentoria>, ResponseEvents> {
 
     @Override
-    public void executeUseCase(RequestCommand requestCommand) {
+    public void executeUseCase(RequestCommand<AgregarDirectrizMentoria> agregarDirectrizMentoriaRequestCommand) {
 
+        var command = agregarDirectrizMentoriaRequestCommand.getCommand();
+        var curso = Curso.from(
+                command.getCursoId(), repository().getEventsBy(command.getCursoId().value())
+        );
+        curso.agregarDirectrizDeMentoria(command.getMentoriaId(), command.getDirectiz());
+
+        emit().onResponse(new ResponseEvents(curso.getUncommittedChanges()));
     }
 }
--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/values/Directiz.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/values/Directiz.java
@@ -2,7 +2,16 @@ package co.com.sofkau.entrenamiento.curso.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class Directiz implements ValueObject<String> {
+
+    private final String value;
+
+    public Directiz(String value){
+        this.value = Objects.requireNonNull(value);
+    }
+   
     @Override
     public String value() {
         return null;
